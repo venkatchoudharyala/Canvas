ac22c8e495e26d146542f181b18d90c5b20bcf24
Venkat Choudhary Ala <venkatchoudhary.ala@gmail.com>
2023-12-30 15:52:06 +0530

    Update AdminPanel.py

--- AdminPanel.py
+++ AdminPanel.py
@@ -14,17 +14,31 @@ hide_st_style = """
 st.markdown(hide_st_style, unsafe_allow_html = True)
 
 def Scrapper():
-  Form = st.form("Login")
-  dir = os.listdir("UserAcc")
-  MPath = st.selectbox("Users", dir)
-  #UserName = Form.text_input("User Name")
-  Path = "UserAcc/" + MPath
-  Rapo(Path)
+	Form = st.form("Login")
+	dir = os.listdir("UserAcc")
+	MPath = st.selectbox("Users", dir)
+	#UserName = Form.text_input("User Name")
+	Path = "UserAcc/" + MPath
+	Rapo(Path)
 def Rapo(Path):
-  try:
-	  with open(Path, "r") as File:
-		  UDetails = File.read()
-		  Details = json.loads(UDetails)
-		  st.write(Details)
-  except FileNotFoundError:
-	  st.write("User Not Found")
+	try:
+		with open(Path, "r") as File:
+			UDetails = File.read()
+			Details = json.loads(UDetails)
+			st.write(Details)
+			DisplayImage(Details["FileName"])
+	except FileNotFoundError:
+		st.write("User Not Found")
+def DisplayImage(path):
+	#DisplayImage(df["IMAGE_DATA_IN_PNG"][0])
+	df = pd.read_excel(path)
+	print(df)
+	
+	for index, row in df.iterrows():
+		text = row["FORMULA_IN_LATEX"]
+		image_base64 = row["IMAGE_DATA_IN_PNG"]
+		image_bytess = base64.b64decode(image_base64)
+		image_io = io.BytesIO(image_bytess)
+		pil_imager = Image.open(image_io)
+		st.write(text)
+		st.image(pil_imager)

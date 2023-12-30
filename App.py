76f62e04bc5805c021241e6627aed85e01858911
Venkat Choudhary Ala <venkatchoudhary.ala@gmail.com>
2023-12-30 13:53:46 +0530

    Update App.py

--- App.py
+++ App.py
@@ -15,6 +15,7 @@ hide_st_style = """
 st.markdown(hide_st_style, unsafe_allow_html = True)
 
 CheckPoint = 349
+FormulaUpdater(CheckPoint)
 def MathFormulae():
 	Formulae = pd.read_excel("MathData/Math_Equations.xlsx")
 	return Formulae.values.tolist()

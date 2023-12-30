e7b8be7139e84afbff484d422c4264d30a0b62b2
Venkat Choudhary Ala <venkatchoudhary.ala@gmail.com>
2023-12-30 15:36:15 +0530

    Update App.py

--- App.py
+++ App.py
@@ -26,63 +26,68 @@ if "user" in st.session_state:
 else:
 	st.session_state["LoginVal"] = False
 
-UserName = UserDetails["Name"]
-
 def MathFormulae():
 	Formulae = pd.read_excel("MathData/Math_Equations.xlsx")
 	return Formulae.values.tolist()
 FormList = MathFormulae()
 
-CheckPoint = 349
-
 def main():
-	if UserName == "Admin":
-		ap.Scrapper()
-	else:
-		# Specify canvas parameters in application
-		drawing_mode = "freedraw"
-		stroke_width = 3
-		stroke_color = "#000000"
-		bg_color = "#eee"
-		realtime_update = True
-		
-		FormulaSpace = st.empty()
-		def FormulaUpdater(CheckPoint):
-			FormulaSpace.latex(FormList[CheckPoint][0])
-		FormulaUpdater(CheckPoint)
-		st.divider()
-		
-		# Create a canvas component
-		canvas_result = st_canvas(
-		    fill_color = "rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
-		    stroke_width = stroke_width,
-		    stroke_color = stroke_color,
-		    background_color = bg_color,
-		    background_image = None,
-		    update_streamlit = realtime_update,
-		    height = 150, 
-		    width = 1000,
-		    drawing_mode = drawing_mode,
-		    point_display_radius = 0,
-		    key = "canvas",
-		)
-		
-		# Image display
-		if canvas_result.image_data is not None:
-			st.image(canvas_result.image_data)
-			if st.button("Save and Proceed"):
-				pil_image = Image.fromarray(canvas_result.image_data)
-				image_bytes = io.BytesIO()
-				pil_image.save(image_bytes, format="PNG")
-				
-				df = pd.read_excel("Files/DrawnImages.xlsx")
-				df.loc[len(df.index)] = {"FORMULA_IN_LATEX": FormList[CheckPoint][0], "IMAGE_DATA_IN_PNG": image_bytes.getvalue()}
-				df.to_excel("Files/DrawnImages.xlsx", index=False)
-		
-				CheckPoint += 1
-				FormulaUpdater(CheckPoint)
-				#st.experimental_rerun()
-				#st.dataframe(df)
+	if st.session_state["LoginVal"]:
+		UserName = UserDetails["Name"]
+		if UserName == "Admin":
+			ap.Scrapper()
+		else:
+			CheckPoint = UserDetails["CompletedCount"]
+			st.latex(FormList[CheckPoint][0])
+			
+			# Specify canvas parameters in application
+			drawing_mode = "freedraw"
+			stroke_width = 3
+			stroke_color = "#000000"
+			bg_color = "#eee"
+			realtime_update = True
+			
+			FormulaSpace = st.empty()
+			def FormulaUpdater(CheckPoint):
+				FormulaSpace.latex(FormList[CheckPoint][0])
+			FormulaUpdater(CheckPoint)
+			st.divider()
+			
+			# Create a canvas component
+			canvas_result = st_canvas(
+			    fill_color = "rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+			    stroke_width = stroke_width,
+			    stroke_color = stroke_color,
+			    background_color = bg_color,
+			    background_image = None,
+			    update_streamlit = realtime_update,
+			    height = 150, 
+			    width = 1000,
+			    drawing_mode = drawing_mode,
+			    point_display_radius = 0,
+			    key = "canvas",
+			)
+			
+			# Image display
+			if canvas_result.image_data is not None:
+				st.image(canvas_result.image_data)
+				if st.button("Save and Proceed"):
+					pil_image = Image.fromarray(canvas_result.image_data)
+					image_bytes = io.BytesIO()
+					pil_image.save(image_bytes, format="PNG")
+					
+					df = pd.read_excel("Files/DrawnImages.xlsx")
+					df.loc[len(df.index)] = {"FORMULA_IN_LATEX": FormList[CheckPoint][0], "IMAGE_DATA_IN_PNG": image_bytes.getvalue()}
+					df.to_excel("Files/DrawnImages.xlsx", index=False)
+
+					UserPath = "UserAcc/" + UserName.strip() + ".ua"
+					with open(UserPath, "r") as File:
+						Details = json.load(File)
+					Details["CompletedCount"] += 1
+					with open(UserPath, "w") as File:
+						json.dump(Details, File)
+					#st.experimental_rerun()
+					#st.dataframe(df)
 
 def DisplayImage():
 	#DisplayImage(df["IMAGE_DATA_IN_PNG"][0])

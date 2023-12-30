0cea07d8440c6154c35983b737c897069575b01a
Venkat Choudhary Ala <venkatchoudhary.ala@gmail.com>
2023-12-30 16:36:12 +0530

    Update AdminPanel.py

--- AdminPanel.py
+++ AdminPanel.py
@@ -40,7 +40,7 @@ def DisplayImage(path):
 	
 	for index, row in df.iterrows():
 		text = row["FORMULA_IN_LATEX"]
-		image_bytes = bytes(row["IMAGE_DATA_IN_PNG"])
+		image_bytes = bytes(row["IMAGE_DATA_IN_PNG"], encoding = 'utf-8')
 		#image_io = io.BytesIO(imag_bytes)
 		#pil_imager = Image.open(image_io)
 		#st.write(text)
